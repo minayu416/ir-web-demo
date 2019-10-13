24efb02c72e7fe7414b7a78aecb3f68d40c90a9d
Ming-Jung YU <mina.yu@kyperdata.com>
2019-10-13 19:21:14 +0800

    change settings value.

--- IR/settings.py
+++ IR/settings.py
@@ -97,7 +97,9 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
